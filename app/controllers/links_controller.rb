8149f60f095bff8db4e5cc10f6e5d4c87780de50
Lucy Fox <lucyreneefox@gmail.com>
2016-09-11 11:42:25 -0600

    Validating link presence of url and title as well as default read value of false

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,6 +4,15 @@ class LinksController < ApplicationController
   end
 
   def create
-    
+    link = Link.new(user: current_user, url: link_params["url"], title: link_params["title"])
+    if link.save
+      
+    else
+    end
+  end
+
+  private
+  def link_params
+    params.require("link").permit(:url, :title)
   end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:url) }
+
+    scenario "defaults to unread" do
+      user = create(:user, email: "thisisanemail@gmail.com")
+      link = Link.create(user: user, url: "https://www.google.com/", title: "My Fav")
+
+      expect(link.read).to eq(false)
+    end
+  end
 end
