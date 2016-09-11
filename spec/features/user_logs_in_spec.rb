0f98ad8dd497b579d22831cd4a23509fca636680
Lucy Fox <lucyreneefox@gmail.com>
2016-09-11 10:41:36 -0600

    Add additional form for log in so user that already exists can log in

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -10,12 +10,14 @@ RSpec.feature "User sees sign on" do
     end
 
     expect(page).to have_current_path(new_user_path)
-  
-    fill_in "Email", with: "lucyreneefox@gmail.com"
-    fill_in "Password", with: "testpassword"
-    fill_in "Password confirmation", with: "testpassword"
 
-    click_button "Create Account"
+    within("#new-sign-up") do
+      fill_in "Email", with: "lucyreneefox@gmail.com"
+      fill_in "Password", with: "testpassword"
+      fill_in "Password confirmation", with: "testpassword"
+
+      click_button "Create Account"
+    end
 
     expect(page).to have_current_path(links_path)
 
@@ -28,7 +30,19 @@ RSpec.feature "User sees sign on" do
   end
 
   scenario "existing user can login" do
-    user = create(:user)
+    user = create(:user, email: "fakemail@mail.com", password: "Password")
+
+    visit '/'
+
+    within ("nav") do
+      click_button "Log In Or Sign Up"
+    end
+
+    within("#log-in") do
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "Password"
+      click_button "Log In"
+    end
 
   end
 end
