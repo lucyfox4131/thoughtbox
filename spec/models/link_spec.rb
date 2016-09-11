1d71282d3cb224dfe3a554748c68cffa69650336
Lucy Fox <lucyreneefox@gmail.com>
2016-09-11 11:48:01 -0600

    Add validate url gem

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe Link, type: :model do
 
       expect(link.read).to eq(false)
     end
+
+    scenario "will not create new link with invalid url" do
+      user = create(:user, email: "thisisanemail@gmail.com")
+      expect(Link.count).to eq(0)
+
+      link = Link.create(user: user, url: "https://www.g", title: "My Fav")
+      expect(Link.count).to eq(0)
+    end
   end
 end
