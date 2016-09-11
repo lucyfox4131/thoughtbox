bd4e4ff491ae22d9214d00a4f64d029ff66bf82b
Lucy Fox <lucyreneefox@gmail.com>
2016-09-11 11:51:22 -0600

    Add regex expression to validate format of url

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :url, presence: true
+  validates_format_of :url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
 end
