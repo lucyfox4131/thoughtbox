9cf1e0e5e0ea73863620fa27ae7aac0ac8594e59
Lucy Fox <lucyreneefox@gmail.com>
2016-09-11 10:07:46 -0600

    User can login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'links/index'
+
   get 'home/show'
 
   root 'home#show'
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create]
+  resources :links, only: [:index]
 end
--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -5,16 +5,21 @@ RSpec.feature "User sees sign on" do
     visit '/'
     expect(page).to have_content("Log In or Sign Up")
 
-    click_button "Sign Up"
+    within ("nav") do
+      click_button "Log In"
+    end
+
     expect(page).to have_current_path(new_user_path)
 
-    fill_in "email", with: "lucyreneefox@gmail.com"
-    fill_in "password", with: "testpassword"
-    fill_in "password-confirmation", with: "testpassword"
+    fill_in "Email", with: "lucyreneefox@gmail.com"
+    fill_in "Password", with: "testpassword"
+    fill_in "Password confirmation", with: "testpassword"
 
-    click_button "Log In"
+    click_button "Sign In"
 
     expect(page).to have_current_path(links_path)
-    expect(page).to have_content("Sign Out")
+    within("nav") do
+      expect(page).to have_button("Log Out")
+    end
   end
 end
