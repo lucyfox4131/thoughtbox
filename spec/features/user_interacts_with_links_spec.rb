4ee4834064c45eac1e71ffbb4f04a5da5e6abca9
Lucy Fox <lucyreneefox@gmail.com>
2016-09-20 14:54:44 -0600

    Fix logical error that was causing test to fail due to repeatable numbers

--- spec/features/user_interacts_with_links_spec.rb
+++ spec/features/user_interacts_with_links_spec.rb
@@ -2,28 +2,26 @@ require 'rails_helper'
 
 RSpec.feature "User views and interacts with links", :js => true do
   let!(:user) { create(:user) }
-  let!(:links) { create_list(:link, 3, user: user) }
+  let!(:links) { create_list(:link, 2, user: user) }
+  let!(:search_link){ create(:link, user: user, title: "SearchableLink")}
 
-  xscenario "Searches links" do
+  scenario "Searches links" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit links_path
     expect(page).to have_content("ThoughtBox")
-    expect(user.links.count).to eq(3)
 
     within(".table") do
       expect(page).to have_content(links.first.title)
       expect(page).to have_content(links.second.title)
-      expect(page).to have_content(links.last.title)
+      expect(page).to have_content(search_link.title)
     end
 
-    first_letter = links.first.title.first
-
-    find("input[name='search[filter]']").set(first_letter)
+    find("input[name='search[filter]']").set("Sea")
 
     within(".table") do
-      expect(page).to have_content(links.first.title)
-      expect(page).to_not have_content(links.second.title)
+      expect(page).to have_content(search_link.title)
+      expect(page).to_not have_content(links.first.title)
       expect(page).to_not have_content(links.last.title)
     end
   end
